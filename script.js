d7b8fc0cfbe05ddcea0940f05fc3df99392ce894
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-01-08 22:32:50 -0500

    coloring depending on time

--- script.js
+++ script.js
@@ -28,25 +28,79 @@ var time = moment();
 
 if (time.isBetween(time8am, time9am)) {
 
+    $("#9am").attr("class","col-lg-9 present")
+    $("#10am").attr("class","col-lg-9 future")
+    $("#11am").attr("class","col-lg-9 future")
+    $("#12pm").attr("class","col-lg-9 future")
+    $("#1pm").attr("class","col-lg-9 future")
+    $("#2pm").attr("class","col-lg-9 future")
+    $("#3pm").attr("class","col-lg-9 future")
+    $("#4pm").attr("class","col-lg-9 future")
+    $("#5pm").attr("class","col-lg-9 future")
+  
+
   console.log('is between')
   console.log(time);
 
 } else if  (time.isBetween(time9am, time10am)) {
   
-    console.log('is between')
+    $("#9am").attr("class","col-lg-9 past")
+    $("#10am").attr("class","col-lg-9 present")
+    $("#11am").attr("class","col-lg-9 future")
+    $("#12pm").attr("class","col-lg-9 future")
+    $("#1pm").attr("class","col-lg-9 future")
+    $("#2pm").attr("class","col-lg-9 future")
+    $("#3pm").attr("class","col-lg-9 future")
+    $("#4pm").attr("class","col-lg-9 future")
+    $("#5pm").attr("class","col-lg-9 future")
+
+    console.log('is between aquiles')
     console.log(time);
 
 } else if (time.isBetween(time10am, time11am)) {
+
+
+    $("#9am").attr("class","col-lg-9 past")
+    $("#10am").attr("class","col-lg-9 past")
+    $("#11am").attr("class","col-lg-9 present")
+    $("#12pm").attr("class","col-lg-9 future")
+    $("#1pm").attr("class","col-lg-9 future")
+    $("#2pm").attr("class","col-lg-9 future")
+    $("#3pm").attr("class","col-lg-9 future")
+    $("#4pm").attr("class","col-lg-9 future")
+    $("#5pm").attr("class","col-lg-9 future")
+ 
 
  console.log('is between')
  console.log(time);
 
 } else if (time.isBetween(time11am, time12pm)) {
 
+    $("#9am").attr("class","col-lg-9 past")
+    $("#10am").attr("class","col-lg-9 past")
+    $("#11am").attr("class","col-lg-9 past")
+    $("#12pm").attr("class","col-lg-9 present")
+    $("#1pm").attr("class","col-lg-9 future")
+    $("#2pm").attr("class","col-lg-9 future")
+    $("#3pm").attr("class","col-lg-9 future")
+    $("#4pm").attr("class","col-lg-9 future")
+    $("#5pm").attr("class","col-lg-9 future")
+
  console.log('is between 12am 1pm')
  console.log(time);
 
 } else if (time.isBetween(time12pm, time1pm)) {
+
+    $("#9am").attr("class","col-lg-9 past")
+    $("#10am").attr("class","col-lg-9 past")
+    $("#11am").attr("class","col-lg-9 past")
+    $("#12pm").attr("class","col-lg-9 past")
+    $("#1pm").attr("class","col-lg-9 present")
+    $("#2pm").attr("class","col-lg-9 future")
+    $("#3pm").attr("class","col-lg-9 future")
+    $("#4pm").attr("class","col-lg-9 future")
+    $("#5pm").attr("class","col-lg-9 future")
+
 
     console.log('is between 12am 1pm')
     console.log(time);
@@ -54,21 +108,62 @@ if (time.isBetween(time8am, time9am)) {
 
 } else if (time.isBetween(time1pm, time2pm)) {
 
+    $("#9am").attr("class","col-lg-9 past")
+    $("#10am").attr("class","col-lg-9 past")
+    $("#11am").attr("class","col-lg-9 past")
+    $("#12pm").attr("class","col-lg-9 past")
+    $("#1pm").attr("class","col-lg-9 past")
+    $("#2pm").attr("class","col-lg-9 present")
+    $("#3pm").attr("class","col-lg-9 future")
+    $("#4pm").attr("class","col-lg-9 future")
+    $("#5pm").attr("class","col-lg-9 future")
+
  console.log('is between 1pm 2pm')
  console.log(time);
 
 } else if (time.isBetween(time2pm, time3pm)) {
+
+    $("#9am").attr("class","col-lg-9 past")
+    $("#10am").attr("class","col-lg-9 past")
+    $("#11am").attr("class","col-lg-9 past")
+    $("#12pm").attr("class","col-lg-9 past")
+    $("#1pm").attr("class","col-lg-9 past")
+    $("#2pm").attr("class","col-lg-9 past")
+    $("#3pm").attr("class","col-lg-9 present")
+    $("#4pm").attr("class","col-lg-9 future")
+    $("#5pm").attr("class","col-lg-9 future")
+
 
  console.log('is between 2pm 3pm')
  console.log(time);
 
 } else if  (time.isBetween(time3pm, time4pm)) {
 
+    $("#9am").attr("class","col-lg-9 past")
+    $("#10am").attr("class","col-lg-9 past")
+    $("#11am").attr("class","col-lg-9 past")
+    $("#12pm").attr("class","col-lg-9 past")
+    $("#1pm").attr("class","col-lg-9 past")
+    $("#2pm").attr("class","col-lg-9 past")
+    $("#3pm").attr("class","col-lg-9 past")
+    $("#4pm").attr("class","col-lg-9 present")
+    $("#5pm").attr("class","col-lg-9 future")
+
+
  console.log('is between 3pm 4pm')
  console.log(time);
 
 } else if (time.isBetween(time4pm, time5pm)) {
 
+    $("#9am").attr("class","col-lg-9 past")
+    $("#10am").attr("class","col-lg-9 past")
+    $("#11am").attr("class","col-lg-9 past")
+    $("#12pm").attr("class","col-lg-9 past")
+    $("#1pm").attr("class","col-lg-9 past")
+    $("#2pm").attr("class","col-lg-9 past")
+    $("#3pm").attr("class","col-lg-9 past")
+    $("#4pm").attr("class","col-lg-9 past")
+    $("#5pm").attr("class","col-lg-9 present")
  console.log('is between 4 pm 5 pm')
  console.log(time);
 
@@ -78,6 +173,17 @@ if (time.isBetween(time8am, time9am)) {
 }
 else {
 
+    $("#9am").attr("class","col-lg-9 past")
+    $("#10am").attr("class","col-lg-9 past")
+    $("#11am").attr("class","col-lg-9 past")
+    $("#12pm").attr("class","col-lg-9 past")
+    $("#1pm").attr("class","col-lg-9 past")
+    $("#2pm").attr("class","col-lg-9 past")
+    $("#3pm").attr("class","col-lg-9 past")
+    $("#4pm").attr("class","col-lg-9 past")
+    $("#5pm").attr("class","col-lg-9 past")
+
+    
 
     console.log('is not between')
     console.log(time);
